c5a8702df5ae120002f5e9ceebe4a04e7738abe2
choong man <rnjscndaksqkq@gmail.com>
2024-01-12 09:47:39 +0900

    파이썬

---
+++ 2024-01-12/001.py
@@ -0,0 +1,10 @@
+# 파이썬 타입 : int,float,str,bool,tuple,list,dictionary,set
+# 파이썬으로 타입 자체를 만들 수 있다 -> 사용자 정의 타입 : class
+# 타입은 필요한 데이터를 저장하고 연산을 수정할 수 있다 
+
+str = 'hello'
+# 파이썬의 모든 타입은 클래스다
+# 파이썬의 모든 변수는 객체다
+# 객체에 소속된 함수를 메소드라고 한다
+print(type(str))
+print(len(str))
